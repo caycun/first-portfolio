7e9a582d90176398ccb9a9e369546344b131adcd
yle11777 <aikencunanan17@gmail.com>
2022-10-14 17:39:46 +0800

    Changed my name and github source code link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ function Footer() {
         <div className="footer">
             <div className="footer-container">
                 <div className="footer-item">
-                    <SideBarIcon icon={<FiAtSign />} /> 2022 Yle_l2c. All rights.reserved.
+                    <SideBarIcon icon={<FiAtSign />} /> 2022 yle11777. All rights.reserved.
                 </div>
                 <div className="footer-item"></div>
                 <div className="footer-item">
-                    <SideBarIcon icon={<FiGithub />} /> <a href="https://github.com/yle-l2c/portfolio">Source Code</a>
+                    <SideBarIcon icon={<FiGithub />} /> <a href="https://github.com/yle11777/portfolio">Source Code</a>
                 </div>
             </div>
         </div>
