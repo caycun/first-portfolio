9e703aaed4cb0c328c3c828e7e6c6521023c273d
Cayle <89197978+caycun@users.noreply.github.com>
2024-10-25 18:11:23 +0800

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,23 +2,17 @@ function Project() {
     return (
         <div className="project-container">
             <ul>
-               <Item
-                   link="https://github.com/forumcord/forumcord" 
-                   linktitle="forumcord/forumcord"
-                   title="forumcord"
-                   description="forumcord is an open-source discord bot where it saves the upcoming forum posts and comments and display it in a web browser."
-                /> 
                  <Item
-                    link="https://github.com/yle-l2c/text-counter"
-                    linktitle="yle11777/text-counter"
+                    link="https://github.com/caycun/text-counter"
+                    linktitle="caycun/text-counter"
                     title="text-counter"
                     description="A tool that counts characters, words, sentences and spaces with your given input and this is my very first react project."
                 />
                 <Item
-                   link="https://github.com/yle11777/portfolio" 
-                   linktitle="yle11777/portfolio"
-                   title="portfolio"
-                   description="The first portfolio that i made with ReactJS, CSS flexbox and love. Feel free to use this for your own but do not forget to give credits."
+                    link="https://youtu.be/TBooOWEB7oQ?si=F2ajYiM9vF2uC-4Y"
+                    linktitle="youtube/link"
+                    title="rejoin tool using discord bot"
+                    description="A rejoin tool controlled using a discord bot for UGPhone."
                 />
             </ul>
         </div>
